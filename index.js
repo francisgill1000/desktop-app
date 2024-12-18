90e30f6c9e0f24ba16e3d299f34ae2fa3378c16f
akildevs1000 <akildevs1000@gmail.com>
2024-12-18 19:34:24 +0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const os = require("os");
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process");
 
 // Function to execute a command with better error handling
 function executeCommand(command, options = {}) {
@@ -58,12 +58,79 @@ Object.keys(networkInterfaces).forEach((interfaceName) => {
 let url = `http://${ipv4Address ?? "localhost"}:${port}`;
 
 let mainWindow;
+let secondaryWindow;
+let workers = null; // Reference to the queue worker process
+const phpPath = `${__dirname}/src/backend/php/php`; // Full path to the PHP executable
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 1980, height: 900 });
   setTimeout(() => {
     mainWindow.loadURL(url);
   }, 5000);
+
+
+  // secondaryWindow = new BrowserWindow({
+  //   width: 800,
+  //   height: 600,
+  //   webPreferences: {
+  //     nodeIntegration: true,
+  //     contextIsolation: false,
+  //   },
+  // });
+
+  // secondaryWindow.loadFile("index.html");
+
+
+  // // Handle starting the worker processes in parallel
+  // ipcMain.on("start-worker", (event, command) => {
+
+  //   if (workers) {
+  //     event.sender.send("error", "Queue Worker is already running.");
+  //     return;
+  //   }
+
+
+  //   // Start the queue worker process
+  //   workers = spawn(phpPath, ["artisan", "queue:work"], {
+  //     cwd: `${__dirname}/src/backend`, // Set working directory to where the Laravel project exists
+  //     shell: true,
+  //   });
+
+  //   workers.stdout.on("data", (data) => {
+  //     event.sender.send("output", `Queue Worker: ${data.toString()}`);
+  //   });
+
+  //   workers.stderr.on("data", (data) => {
+  //     event.sender.send("error", `Queue Worker Error: ${data.toString()}`);
+  //   });
+
+  //   workers.on("close", (code) => {
+  //     event.sender.send(
+  //       "exit",
+  //       code === 0
+  //         ? "Queue Worker exited successfully."
+  //         : `Queue Worker exited with code ${code}`
+  //     );
+  //     workers = null; // Reset queue worker process when the process exits
+  //   });
+  // });
+
+  // // Handle stopping both worker processes
+  // ipcMain.on("stop-worker", (event) => {
+  //   let stopped = false;
+
+  //   // Stop queue worker if running
+  //   if (workers) {
+  //     workers.kill();
+  //     workers = null;
+  //     event.sender.send("exit", "Queue Worker process stopped.");
+  //     stopped = true;
+  //   }
+
+  //   if (!stopped) {
+  //     event.sender.send("error", "No worker process is running.");
+  //   }
+  // });
 }
 
 // When Electron is ready, start the processes
